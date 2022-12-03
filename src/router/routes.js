2a447ca85311529a8f15a87eb7f920b655d39878
krtninja <kitonashi@yandex.ru>
2022-12-03 17:45:03 +0300

    Страница регистрации

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,12 @@
 import { Chats } from '../pages/chats/Chats';
 import { Error } from '../pages/error/Error';
 import { Login } from '../pages/login/Login.js';
+import { SignIn } from '../pages/signin/Signin.js';
 
 export const routes = {
   '/': Chats,
   '/login': Login,
+  '/signin': SignIn,
   '/404': Error,
   '/500': () => Error({ code: '500', message: 'Мы уже фиксим' })
 };
