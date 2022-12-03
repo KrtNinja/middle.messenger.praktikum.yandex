7a9ec1ac3ac048b03ed19148d7a67f6c8948d1a2
krtninja <kitonashi@yandex.ru>
2022-12-03 20:29:51 +0300

    Добавил возможность изменять стили у кнопки через пропы

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,6 +2,23 @@ import tmpl from './button.tmpl.hbs';
 import Handlebars from 'handlebars';
 import './button.styles.css';
 
+const classByVariant = {
+  contained: 'LWButton--contained',
+  text: 'LWButton--text'
+};
+
+const classByColor = {
+  white: 'LWButton--white',
+  primary: 'LWButton--primary',
+  error: 'LWButton--error'
+};
+
 export const registerButton = () => {
-  Handlebars.registerPartial('LWButton', tmpl);
+  Handlebars.registerPartial('LWButton', props => {
+    props.classes = `${classByVariant[props?.variant ?? 'contained']} ${
+      classByColor[props?.color ?? 'white']
+    }`;
+
+    return tmpl(props);
+  });
 };
