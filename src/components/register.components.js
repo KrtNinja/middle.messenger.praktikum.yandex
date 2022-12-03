08f6d6881c388582ef87bd25e4bcc8868bf0b41f
krtninja <kitonashi@yandex.ru>
2022-12-03 16:11:34 +0300

    Добавил компонент Input

--- src/components/register.components.js
+++ src/components/register.components.js
@@ -1,5 +1,7 @@
 import { registerLayout } from './Layout/Layout.js';
 import { registerButton } from './Button/Button.js';
+import { registerInput } from './Input/Input.js';
 
 registerLayout();
 registerButton();
+registerInput();
