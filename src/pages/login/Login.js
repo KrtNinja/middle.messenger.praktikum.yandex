4177d0a49ed92b5c46283c38496cd7190dee9da0
krtninja <kitonashi@yandex.ru>
2022-12-03 17:06:32 +0300

    Страница авторизации

---
+++ src/pages/login/Login.js
@@ -0,0 +1,4 @@
+import template from './login.tmpl.hbs';
+import './login.styles.css';
+
+export const Login = () => template();
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,10 @@
 import { Chats } from '../pages/chats/Chats';
 import { Error } from '../pages/error/Error';
+import { Login } from '../pages/login/Login.js';
 
 export const routes = {
   '/': Chats,
+  '/login': Login,
   '/404': Error,
-  '/500': () => Error({code: '500', message: 'Мы уже фиксим'})
+  '/500': () => Error({ code: '500', message: 'Мы уже фиксим' })
 };
